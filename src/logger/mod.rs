dcf1097dceb8961f50fbd408f32e959264d488a6
Ethan Gilles <ethangilles8@gmail.com>
2023-09-04 15:41:37 -0400

    Started on Logger

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -1,7 +1,11 @@
-/*use std::fs::File;
+use std::{fs::{File, self, ReadDir}, io::{Write, Error}};
+
+use chrono::Local;
 
 
-//static mut initalized: bool = false;
+const INFO: &str = " [Info]: ";
+const WARN: &str = " [Warn]: ";
+const DIRECTORY: &str = "DataLogs/";
 
 pub struct Logger {
     pub stream: File,
@@ -9,24 +13,64 @@ pub struct Logger {
 
 impl Logger {
     pub fn initalize() -> Logger {
-        match File::open("") {
-            Ok(file) => {},
-            Err(_) => {},
-        }
-        Logger {
-            stream: file,
-        }
+        let mut output: Logger = Logger {
+            stream: create_log_file().unwrap(),
+        };
+        output.log_info("Logger Started.");
+
+        output
+    }
+
+    pub fn log_time(&mut self) {
+        self.stream.write_all(
+            Local::now()
+            .format("[%F %H:%M:%S%.3f]")
+            .to_string()
+            .as_bytes()
+        ).expect("Error logging time.");
+    }
+
+    pub fn log_info(&mut self, message: &str) {
+        self.log_time();
+        self.stream.write_all(
+            format!("{INFO}{message}\n")
+            .as_bytes()
+        ).expect("Error logging info.");
+    }
+
+    pub fn log_warn(&mut self, message: &str) {
+        self.log_time();
+        self.stream.write_all(
+            format!("{WARN}{message}\n")
+            .as_bytes()
+        ).expect("Error logging info.");
     }
 }
 
 
 impl Drop for Logger {
     fn drop(&mut self) {
-    
         println!("Exiting.");
     }
 }
 
-fn find_index() -> u32 {
-    File::
-}*/
+fn create_log_file() -> Result<File, Error> {
+    let directory: ReadDir = loop {
+        match fs::read_dir(DIRECTORY) {
+            Err(_) => {
+                fs::create_dir(DIRECTORY)?;
+                // Adds "DataLogs/" to the .gitignore file.
+                File::options()
+                    .append(true)
+                    .open(".gitignore")
+                    ?.write_all(DIRECTORY.as_bytes())?;
+                continue;
+            },
+            Ok(dir) => break dir,
+        }
+    };
+
+    let file_number: usize = directory.count();
+    
+    File::create(format!("{DIRECTORY}log{file_number}.log"))
+}
